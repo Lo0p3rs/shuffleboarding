f23cfec1b2dfa2f6ca9f84d0c7a0bd5b25c17db1
Rishi N. <rishnir27@gmail.com>
2024-03-29 16:00:56 -0700

    Added more

--- example-plugins/note-selector/src/main/java/com/martians/noteselector/widget/NoteSelector.java
+++ example-plugins/note-selector/src/main/java/com/martians/noteselector/widget/NoteSelector.java
@@ -1,7 +1,6 @@
 package com.martians.noteselector.widget;
 
 import java.util.concurrent.Callable;
-
 import edu.wpi.first.shuffleboard.api.widget.Description;
 import edu.wpi.first.shuffleboard.api.widget.ParametrizedController;
 import edu.wpi.first.shuffleboard.api.widget.SimpleAnnotatedWidget;
@@ -13,12 +12,15 @@ import javafx.fxml.FXML;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.Slider;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.ToggleButton;
 import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseButton;
+import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.Background;
 import javafx.scene.layout.BackgroundFill;
 import javafx.scene.layout.BackgroundImage;
@@ -48,6 +50,9 @@ public final class NoteSelector extends SimpleAnnotatedWidget<Boolean> {
   private Pane root;
   @FXML
   public Button anButton, snButton, srnButton, f1nButton, f2nButton, f3nButton, f4nButton, f5nButton, startButton;
+
+  @FXML
+  public Slider noteSlider;
   
   @FXML
   public TableView<Item> noteTbl;
@@ -56,6 +61,7 @@ public final class NoteSelector extends SimpleAnnotatedWidget<Boolean> {
 
   public String[] notes = new String[] {"Note 1", "Note 2", "Note 3", "Note 4", "Note 5", "Note 6", "Note 7", "Note 8"};
   public int arrayVal;
+  public int arrayLimit;
 
 
   @FXML
@@ -76,6 +82,7 @@ public final class NoteSelector extends SimpleAnnotatedWidget<Boolean> {
       removeButtonState(f3nButton);
       removeButtonState(f4nButton);
       removeButtonState(f5nButton);
+      arrayLimit = (int) noteSlider.getValue(); //TODO: move to proper start button
     });
 
     initializeButtons("Amp Note", anButton);
@@ -86,6 +93,12 @@ public final class NoteSelector extends SimpleAnnotatedWidget<Boolean> {
     initializeButtons("Far 3 Note", f3nButton);
     initializeButtons("Far 4 Note", f4nButton);
     initializeButtons("Far 5 Note", f5nButton);
+
+    startButton.setOnMouseDragOver(event -> {
+       
+    });
+
+    arrayLimit = (int) noteSlider.getValue();
   
   }
 
@@ -114,11 +127,26 @@ public final class NoteSelector extends SimpleAnnotatedWidget<Boolean> {
       noteTbl.getItems().addAll(new Item(notes[arrayVal], noteName));
       arrayVal += 1;
       button.setDisable(true);
+
+      if(arrayVal == arrayLimit){
+          disableAllButtons();
+      }
     });
   }
 
   public void removeButtonState(Button button){
     button.setDisable(false);
+  }
+
+  public void disableAllButtons(){
+    anButton.setDisable(true);
+    snButton.setDisable(true);
+    srnButton.setDisable(true);
+    f1nButton.setDisable(true);
+    f2nButton.setDisable(true);
+    f3nButton.setDisable(true);
+    f4nButton.setDisable(true);
+    f5nButton.setDisable(true);
   }
 
   @Override
